1903a4f9155182c6a16ddecc0bdc712efb5b3896
Derek Stegelman <dstegelman@gmail.com>
2025-12-09 14:27:26 -0700

    Problem details handler

---
+++ CsuChhs.Blazor/Objects/PageProblemDetails.cs
@@ -0,0 +1,19 @@
+﻿namespace CsuChhs.Blazor.Objects;
+
+public class PageProblemDetails
+{
+    public PageProblemDetails()
+    {
+        IsError = false;
+        Message = string.Empty;
+    }
+
+    public PageProblemDetails(string message)
+    {
+        IsError = true;
+        Message = message;
+    }
+    
+    public bool IsError { get; init; }
+    public string Message { get; init; }
+}
